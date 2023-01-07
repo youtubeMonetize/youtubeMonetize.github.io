8670e717388f8a328348184d6b9ee968a70bbacb
youtubeMonetize <122079384+youtubeMonetize@users.noreply.github.com>
2023-01-08 00:34:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 122
-
+const searchString = req.query.search;
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+${searchString ? '<h2> You searched </h2>'}
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
