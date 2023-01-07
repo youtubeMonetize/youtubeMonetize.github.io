fe1ead5a520cd500a5f29a8aa797aeaa3d685763
youtubeMonetize <122079384+youtubeMonetize@users.noreply.github.com>
2023-01-08 00:36:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 122
-const searchString = req.query.search;
+const searchString = req.query.user_name;
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
