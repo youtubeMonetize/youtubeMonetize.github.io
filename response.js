422d1f48718ec54dff19b7714eba95cdb591940c
youtubeMonetize <122079384+youtubeMonetize@users.noreply.github.com>
2023-01-07 12:10:15 +0300

    Update response.js

--- response.js
+++ response.js
@@ -1,11 +1,3 @@
-const express = require('express')
-const app = express()
-const port = 122
-
-app.get('/', (req, res) => {
+index.get('/', (req, res) => {
   res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
 })
