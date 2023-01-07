164a35c66764cbc1b1a59a5fe8ecea3803333220
youtubeMonetize <122079384+youtubeMonetize@users.noreply.github.com>
2023-01-08 00:42:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,57 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 ${searchString ? '<h2> You searched </h2>'}
+
+
+  
+  <!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Is Monetize?</title>
+	<style>
+	form{
+	margin: 0 auto;
+	width: 500px;
+	padding: 1em;
+	border: 1px solid #ccc
+	border-radius: 1em;
+	}
+	h1{
+	margin: 0 auto;
+	width: 600px;
+	padding: 1em;
+	border: 1px solid #ccc
+	border-radius: 1em;
+	}
+	button{
+	margin-left: 0.5em;
+	
+	}
+	
+	
+
+	</style>
+  </head>
+  <body>
+	
+    	<h1>IS MY CHANNEL MONETIZED?</h1>
+	<form action = "/" method = "get"> 
+	
+		
+		<label for="name">Enter your you tube username:</label>
+
+		<input type="text" id="name" name="user_name" />
+	
+		<class="button">
+  		<button type="submit">Enter</button>	
+	
+	</form> 
+
+  </body>
+</html>
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
