3498d01d75920ddec339956cf50e6fffe146f2a0
youtubeMonetize <122079384+youtubeMonetize@users.noreply.github.com>
2023-01-08 12:20:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const port = 8080;
 
-app.use(bodyParser.urlencoded({ extended: true })); 
 
-app.post('/', (req, res) => {
+
+app.get('/', (req, res) => {
   res.send(`hello world`);
 });
 
